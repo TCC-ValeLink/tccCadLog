a93b6d52cbdf0dea57c76f330d0b6d1b15c63029
Art0_alb <arthuralbinot844@gmail.com>
2024-10-02 15:27:59 -0300

    Nada demais

--- view/home.php
+++ view/home.php
@@ -1,24 +1,27 @@
-<?php Include("blades/top.php")?>
-<?php Include("blades/src.php")?>
-    <div class="body-home">
-        <div class="center">
-            <div class="content-post">
-                <div class="top-post">
-                    <div class="foto-profile"></div>
-                    <div class="nome-profile">Nome Aleatório</div>
-                    <div class="data-post">Data Aleatória</div>
-                    <button type="submit" id="follow" value="0" function="" name="btnFollow" class="btnFollow">Seguir</button>
-                </div>
-                <div class="conteudo-post">Postagem aleatória</div>
-                <div class="midia-post">Foto Aleatória</div>
-                <div class="footer-post">
-                    <div class="icones">A</div>
-                    <button type="submit" name="btnLike" id="Like" value="0" function="">Coração</button>
-                </div>
+<?php include("blades/top.php") ?>
+<?php include("blades/src.php") ?>
+<div class="body-home">
+    <div class="center">
+        <div class="content-post">
+            <div class="top-post">
+                <div class="foto-profile"></div>
+                <div class="nome-profile">@contaEmpresa</div>
+                <div class="data-post">Data Aleatória</div>
+                <button type="submit" id="follow" value="0" function="" name="btnFollow"
+                    class="btnFollow">Seguir</button>
+            </div>
+            <div class="conteudo-post">Postagem da Empresa</div>
+            <div class="midia-post">Mídia do Post</div>
+            <div class="footer-post">
+                <div class="icones">A</div>
+                <button type="submit" name="btnLike" id="Like" value="0" onclick="">♡</button>
+                <button type="submit" class="btnCur" name="btnCur" id="Like" value="0" onclick="">Enviar
+                    Currículo</button>
             </div>
         </div>
     </div>
+</div>
 
-<?php Include("blades/followemp.php")?>
-<?php Include("blades/menu.php")?>
-<?php Include("blades/footercomp.php")?>
+<?php include("blades/followemp.php") ?>
+<?php include("blades/menu.php") ?>
+<?php include("blades/footercomp.php") ?>
